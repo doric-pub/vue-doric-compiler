14a15f9160f0c17028720d11fba47bbf96a0cb04
Unknown <jingpeng.wang@outlook.com>
2022-07-14 17:59:48 +0800

    add source for compile

--- src/vue/compiler/directives/index.ts
+++ src/vue/compiler/directives/index.ts
@@ -1,9 +1,9 @@
-import on from "./on";
-import bind from "./bind";
-import { noop } from "vue/src/shared/util";
+import on from './on'
+import bind from './bind'
+import { noop } from 'shared/util'
 
 export default {
   on,
   bind,
-  cloak: noop,
-};
+  cloak: noop
+}
--- src/vue/compiler/directives/on.ts
+++ src/vue/compiler/directives/on.ts
@@ -1,9 +1,9 @@
-import { warn } from "vue/src/core/util/index";
-import { ASTDirective, ASTElement } from "vue/src/types/compiler";
+import { warn } from 'core/util/index'
+import { ASTDirective, ASTElement } from 'types/compiler'
 
 export default function on(el: ASTElement, dir: ASTDirective) {
   if (__DEV__ && dir.modifiers) {
-    warn(`v-on without argument does not support modifiers.`);
+    warn(`v-on without argument does not support modifiers.`)
   }
-  el.wrapListeners = (code: string) => `_g(${code},${dir.value})`;
+  el.wrapListeners = (code: string) => `_g(${code},${dir.value})`
 }
