5f5a56303e8722243c4518b853b8c22d80249def
王劲鹏 <jingpeng.wang@outlook.com>
2022-07-27 18:03:22 +0800

    add examples

---
+++ examples/Test.vue
@@ -0,0 +1,33 @@
+<template>
+  <view>
+    <view @tap="click" class="abc">
+      <text style="color: green;font-size: 50px;">{{ !a ? '打开': '关闭'}}</text>
+    </view>
+    <view v-if="a">
+      <text style="color: #ff4800;font-size: 50px;">11111</text>
+    </view>
+    <text v-for="(item, i) in list">{{i}}---{{item}}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: true,
+      list: [1, 2, 3, 4, 5]
+    }
+  },
+  methods: {
+    click() {
+      this.a = !this.a
+    }
+  }
+}
+</script>
+
+<style scoped>
+.abc {
+  background-color: #f0cc00;
+}
+</style>
